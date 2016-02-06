3eddebfcb4eb8a4f0da5cb383c43281aee7496d6
ripnraks300 <ErickTruong@macbook-pro-2.home>
2016-02-05 23:35:58 -0500

    Initial Commit

---
+++ bowl-social-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bowl-social-dev
+//
+//  Created by ERICK TRUONG on 2/5/16.
+//  Copyright © 2016 Code Create Innovate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
